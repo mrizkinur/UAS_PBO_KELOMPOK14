053b9105ea8bece628b186a91fecc16d265d30d3
RaphaelArmandoJose <129347348+RaphaelArmandoJose@users.noreply.github.com>
2023-06-26 16:51:44 +0700

    Delete Interface.java

--- UAS PBO/Interface.java
+++
@@ -1,9 +0,0 @@
-package Harga;
-
-public abstract interface Interface{
-    public int topping();
-    public int filing();
-    public int modal();
-    public int harga_varian();
-    public int harga_isian();
-}
