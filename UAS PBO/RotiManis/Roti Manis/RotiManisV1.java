36a536a8c1d3c0f9e6a6d40a1a9865912cd23c5c
Zidanf12 <112364468+Zidanf12@users.noreply.github.com>
2023-06-26 08:49:33 +0700

    Add files via upload

---
+++ UAS PBO/RotiManis/Roti Manis/RotiManisV1.java
@@ -0,0 +1,39 @@
+
+package rotimanis;
+import modal.RotiManis;
+import harga.Interface;
+
+public class RotiManisV1 extends RotiManis implements Interface {
+    
+    String nama = "Roti Manis Varian 1";
+    
+    public double topping() {
+        double coklat  = this.hargaCoklat() * 5;
+        double harga = coklat;
+        return harga;
+    }
+
+    public double filling() {
+        double keju = this.hargaKeju() * 5;
+        double coklat  = this.hargaCoklat() * 5;
+        double harga = keju + coklat;
+        return harga;
+    }
+
+    public double modal() {
+        double hargaTopFill = this.filling() + this.topping();
+        double hargaBahan = this.hitungModal() / this.rotiPerAdonan();
+        double modalTotal = hargaBahan + hargaTopFill;
+        return modalTotal;
+    }
+    
+    public double hargaVarian(){
+        double untung = this.modal() * 50/100;
+        double harga = this.modal() + untung;
+        return Math.round(harga);
+    }
+    
+    public void harga(){
+        System.out.println("Harga " + nama  + " = Rp " + (int)hargaVarian() + " /pcs");
+    }
+}
---
+++ UAS PBO/RotiManis/Roti Manis/RotiManisV3.java
@@ -0,0 +1,37 @@
+
+package rotimanis;
+import modal.RotiManis;
+import harga.Interface;
+
+public class RotiManisV3 extends RotiManis implements Interface {
+    String nama = "Roti Manis Varian 3";
+    
+    public double filling() {
+        double keju = this.hargaKeju() * 10;
+        double sosis  = this.hargaCoklat() * 10;
+        double harga = keju + sosis;
+        return harga;
+    }
+
+    public double modal() {
+        double hargaTopFill = this.filling();
+        double hargaBahan = this.hitungModal() / this.rotiPerAdonan();
+        double modalTotal = hargaBahan + hargaTopFill;
+        return modalTotal;
+    }
+    
+    public double hargaVarian(){
+        double untung = this.modal() * 50/100;
+        double harga = this.modal() + untung;
+        return Math.round(harga);
+    }
+    
+    public void harga(){
+        System.out.println("Harga " + nama  + " = Rp " + (int)hargaVarian() + " /pcs");
+    }
+
+    public double topping() {
+        return 0.0;
+    }
+    
+}
