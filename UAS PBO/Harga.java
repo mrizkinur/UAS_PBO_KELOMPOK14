4af1d267057ed3e5dba05e412a1a5c7613ca3a1c
RaphaelArmandoJose <129347348+RaphaelArmandoJose@users.noreply.github.com>
2023-06-26 16:49:34 +0700

    Harga

---
+++ UAS PBO/Harga.java
@@ -0,0 +1,63 @@
+package Harga;
+
+import Komposisi.TopFill;
+
+public class Harga implements TopFill{
+    
+    //Harga Bahan
+    int Harga_TepungTerigu(){
+        return 40000;
+    }
+    int Harga_GulaPasir(){
+        return 25000;
+    }
+    int Harga_Butter(){
+        return 23000;
+    }
+    int Harga_Ragi(){
+        return 5000;
+    }
+    int Harga_SusuBubuk(){
+        return 39000;
+    }
+    int Harga_SusuCair(){
+        return 24000;
+    }
+    int Harga_Telur(){
+        return 23000;
+    }
+    int Harga_EsBatu(){
+        return 2000;
+    }
+
+    //Harga Topping
+   
+    @Override
+    public double Keju() {
+        return 30000;
+    }
+    @Override
+    public double Coklat() {
+        return 29000;
+    }
+    @Override
+    public double KrimVanilla() {
+        return 30000;
+    }
+    @Override
+    public double SelaiRedbean() {
+        return 25000;
+    }
+    @Override
+    public double Sosis() {
+        return 80000;
+    }
+    @Override
+    public double SmokedBeef() {
+        return 90000;
+    }
+    @Override
+    public double BawangBombay() {
+        return 40000;
+    } 
+}    
