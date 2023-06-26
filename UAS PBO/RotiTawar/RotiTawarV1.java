fc4f255c138ea8bdc803e543d2756efa93db1b52
aaadiknh <adiknurhalimah@gmail.com>
2023-06-26 19:08:15 +0700

    Roti Tawar
    Roti Tawar varian 1 dan 2

--- UAS PBO/RotiTawar/RotiTawarV1.java
+++ UAS PBO/RotiTawar/RotiTawarV1.java
@@ -1,5 +1,39 @@
 package RotiTawar;
+import Modal.RotiTawar;
+import Harga.Interface;
 
-public class RotiTawarV1{
+public class RotiTawarV1 extends RotiTawar implements Interface {
+    
+    String nama = "Roti Tawar Varian 1";
+    
+    @Override
+    public double topping () {
+        //menghitung topping roti tawar varian 1
+        return 0;
+    }
 
+    @Override
+    public double filling() {
+        double coklat  = this.hargaCoklat() * 80;
+        double harga = coklat;
+        return harga;
+    }
+
+    @Override
+    public double modal() {
+        double hargaTopFill = this.filling() + this.topping();
+        double hargaBahan = this.hitungModal() / this.rotiPerAdonan();
+        double totalModal = hargaBahan + hargaTopFill;
+        return totalModal;
+    }
+    
+    public double hargaVarian(){
+        double untung = this.modal() * 50/100;
+        double harga = this.modal() + untung;
+        return Math.round(harga);
+    }
+    
+    public void harga(){
+        System.out.println("Harga " + nama  + " = Rp " + (int)hargaVarian() + " /pcs");
+    }
 }
--- UAS PBO/RotiTawar/RotiTawarV2.java
+++ UAS PBO/RotiTawar/RotiTawarV2.java
@@ -1,5 +1,39 @@
-package RotiTawar; 
+packageackage RotiTawar;
+import Modal.RotiTawar;
+import Harga.Interface;
 
-public class RotiTawarV2{
+public class RotiTawarV2 extends RotiTawar implements Interface {
+    
+    String nama = "Roti Tawar Varian 2";
+    
+    @Override
+    public double topping () {
+        //menghitung topping roti tawar varian 1
+        return 0;
+    }
 
+    @Override
+    public double filling() {
+        double keju  = this.hargaKeju() * 80;
+        double harga = keju;
+        return harga;
+    }
+
+    @Override
+    public double modal() {
+        double hargaTopFill = this.filling() + this.topping();
+        double hargaBahan = this.hitungModal() / this.rotiPerAdonan();
+        double totalModal = hargaBahan + hargaTopFill;
+        return totalModal;
+    }
+    
+    public double hargaVarian(){
+        double untung = this.modal() * 50/100;
+        double harga = this.modal() + untung;
+        return Math.round(harga);
+    }
+    
+    public void harga(){
+        System.out.println("Harga " + nama  + " = Rp " + (int)hargaVarian() + " /pcs");
+    }
 }
