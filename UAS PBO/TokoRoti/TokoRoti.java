04028dae1771c8336bf24e34084391c607afbfc9
Zidanf12 <112364468+Zidanf12@users.noreply.github.com>
2023-06-26 19:48:54 +0700

    Update TokoRoti.java

--- UAS PBO/TokoRoti/TokoRoti.java
+++ UAS PBO/TokoRoti/TokoRoti.java
@@ -1,7 +1,16 @@
 package TokoRoti;
+import rotimanis.*;
+import rotitawar.*;
+import pizza.*;
+import beban.BebanBahan;
 
 public class TokoRoti{
     public static void main(String[] args) {
-        
+        RotiManisV1 rm1 = new RotiManisV1();
+        RotiManisV2 rm2 = new RotiManisV2();
+        RotiManisV3 rm3 = new RotiManisV3();
+        RotiTawarV1 rt1 = new RotiTawarV1();
+        RotiTawarV2 rt2 = new RotiTawarV2();
+        PizzaV1 p1  = new PizzaV1();
         }
 }
